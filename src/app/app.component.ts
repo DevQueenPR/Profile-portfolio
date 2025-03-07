2325dd5b08811f59e9dd6afcc53129beb3e10147
genesis ojeda <genesismojeda@gmail.com>
2025-03-07 19:07:41 -0400

    finished pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RouterModule } from '@angular/router';
 import { ExtracurricularsComponent } from "./profile/extracurriculars/extracurriculars.component";
+import { TypingEffectComponent } from './typing-effect/typing-effect.component';
 import routes from './app.routes';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, ProfileComponent, RouterModule, ExtracurricularsComponent],
+  imports: [RouterOutlet, NavbarComponent, ProfileComponent, RouterModule, ExtracurricularsComponent, TypingEffectComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
